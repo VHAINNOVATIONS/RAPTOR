3a3cf5cfd3472a8289738f79d0f663a0c210538a
frankfont <frankfont123@gmail.com>
2014-11-08 14:35:38 -0500

    November 2014 Snapshot

--- bitnamistack/trunk/apps/drupal/htdocs/sites/all/themes/raptor_omega/js/userform.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function () {
-	// Generic Cancel button click handler
-	$('.admin-cancel-button').on('click', function (e) {
-		location.href = $(this).attr('data-redirect');
-	});
-});
--- bitnamistack/trunk/apps/drupal/htdocs/sites/all/themes/raptor_omega/layouts/render-footer.php
+++
@@ -1,3 +0,0 @@
-<footer>
-VA systems are intended to be used by authorized VA network users for view and retrieving information only except as otherwise explicitly authorized for official business and limited personal use under VA policy.  Information from this system resides on and transmits through computer systems and networks funded by the VA.
-</footer>
