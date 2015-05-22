c24ff8624e48d5a07a33ae8c4f19283f4ace4287
Frank Font <frankfont123@gmail.com>
2015-05-22 10:01:49 -0400

    refactored includes

--- modules/raptor_glue/core/config.php
+++ modules/raptor_glue/core/config.php
@@ -1,18 +1,29 @@
 <?php
 
-//require_once '32bitVM_local_oldMDWS.inc';
+defined("RAPTOR_BUILD_ID")
+    or define("RAPTOR_BUILD_ID", 'Test Candidate 20150522.2');
+
+/*
+ * We require one of the following instance specific includes.
+ */
+//require_once 'RSite500_146.inc';
+//require_once 'RSite500_147.inc';
+//require_once 'RSite970_146.inc';
+//require_once 'RSite970_147.inc';
+//require_once 'RSite978_146.inc';
+//require_once 'RSite978_147.inc';
+//require_once 'RSite948_146.inc';
+//require_once 'RSite948_147.inc';
+//require_once 'RSite963_146.inc';
+//require_once 'RSite963_147.inc';
 //require_once '32bitVM_local_newMDWS.inc';
-//require_once '64bitVM_SETUP203-4.inc';
-//require_once '64bitVM_SETUP203-5.inc';
-//require_once '64bitVA_146.inc';
-//require_once '64bitVA_147.inc';
 //require_once '64bitVM_184.inc';
 //require_once '64bitVM_54-193.inc';
 require_once '64bitVM_local.inc';
 
-defined("RAPTOR_BUILD_ID")
-    or define("RAPTOR_BUILD_ID", 'Test Candidate 20150519.1');
-
+/*
+ * We require all of the following.
+ */
 require_once 'InternalDefs.inc';
 require_once 'TimeDefs.inc';
 require_once 'VistaDefs.inc';
