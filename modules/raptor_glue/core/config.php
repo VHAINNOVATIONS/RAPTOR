7007ecb7454d0acd414aa007ef19671a97f0e9a3
Frank Font <frankfont123@gmail.com>
2015-05-30 12:09:35 -0400

    cleanup

--- modules/raptor_glue/core/config.php
+++ modules/raptor_glue/core/config.php
@@ -1,7 +1,7 @@
 <?php
 
 defined("RAPTOR_BUILD_ID")
-    or define("RAPTOR_BUILD_ID", 'Test Candidate 20150528.3');
+    or define("RAPTOR_BUILD_ID", 'Test Candidate 20150530.1');
 
 /*
  * We require one of the following instance specific includes.
