80112ea3a15c25cc29872091e393c267f72f8428
Frank Font <frankfont123@gmail.com>
2015-06-04 16:04:41 -0400

    added base url into log messages

--- modules/raptor_glue/core/config.php
+++ modules/raptor_glue/core/config.php
@@ -1,7 +1,7 @@
 <?php
 
-defined("RAPTOR_BUILD_ID")
-    or define("RAPTOR_BUILD_ID", 'Test Candidate 20150604.1');
+defined('RAPTOR_BUILD_ID')
+    or define('RAPTOR_BUILD_ID', 'Test Candidate 20150604.2');
 
 /*
  * We require one of the following instance specific includes.
