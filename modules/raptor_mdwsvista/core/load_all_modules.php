c2f95bec7461fe33f26b5fcfd45fafd48e745b81
Frank Font <frankfont123@gmail.com>
2015-07-13 13:44:57 -0400

    refactoring mdws integration

--- modules/raptor_mdwsvista/core/load_all_modules.php
+++ modules/raptor_mdwsvista/core/load_all_modules.php
@@ -12,20 +12,8 @@
  * 
  */ 
 
-module_load_include('php', 'raptor_mdwsvista', 'core/load_all_modules');
-
-/*
-module_load_include('php', 'raptor_mdwsvista', 'core/MdwsDao');
-module_load_include('php', 'raptor_mdwsvista', 'core/MdwsDaoFactory');
-module_load_include('php', 'raptor_mdwsvista', 'core/MdwsNewOrderUtils');
-module_load_include('php', 'raptor_mdwsvista', 'core/MdwsUserUtils');
-module_load_include('php', 'raptor_mdwsvista', 'core/MdwsUtils');
- * 
 require_once 'MdwsDao.php';
 require_once 'MdwsDaoFactory.php';
 require_once 'MdwsNewOrderUtils.php';
 require_once 'MdwsUserUtils.php';
 require_once 'MdwsUtils.php';
-*/
-
-
