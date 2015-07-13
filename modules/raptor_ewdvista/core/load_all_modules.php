3952676590c812b55432d3043dd5e709351d11fd
Frank Font <frankfont123@gmail.com>
2015-07-13 13:51:42 -0400

    creating stubs

---
+++ modules/raptor_ewdvista/core/load_all_modules.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @file
+ * ------------------------------------------------------------------------------------
+ * Created by SAN Business Consultants for RAPTOR phase 2
+ * Open Source VA Innovation Project 2011-2015
+ * VA Innovator: Dr. Jonathan Medverd
+ * SAN Implementation: Andrew Casertano, Frank Font, et al
+ * MDWS Integration and VISTA collaboration: Joel Mewton
+ * Contacts: acasertano@sanbusinessconsultants.com, ffont@sanbusinessconsultants.com
+ * ------------------------------------------------------------------------------------
+ * 
+ */ 
+
+//TBD
