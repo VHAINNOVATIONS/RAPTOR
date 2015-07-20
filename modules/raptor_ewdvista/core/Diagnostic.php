a5091a92f69e0be5eb9df818271e89621f3f57f7
Frank Font <frankfont123@gmail.com>
2015-07-20 11:21:02 -0400

    stub for core diagnostic code

---
+++ modules/raptor_ewdvista/core/Diagnostic.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * @file
+ * ------------------------------------------------------------------------------------
+ * Created by SAN Business Consultants for RAPTOR phase 2
+ * Open Source VA Innovation Project 2011-2015
+ * VA Innovator: Dr. Jonathan Medverd
+ * SAN Implementation: Andrew Casertano, Frank Font, et al
+ * Contacts: acasertano@sanbusinessconsultants.com, ffont@sanbusinessconsultants.com
+ * ------------------------------------------------------------------------------------
+ * 
+ */ 
+
+namespace raptor_ewdvista;
+
+/**
+ * This is the primary interface implementation to diagnostic code
+ *
+ * @author Frank Font of SAN Business Consultants
+ */
+class Diagnostic
+{
+    //TODO core diagnostic functions here
+}
