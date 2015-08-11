d8a994fa50e790cbe7e4b20602600b75c8f2ead3
Frank Font <frankfont123@gmail.com>
2015-08-11 09:02:07 -0400

    latest code with joel

--- EWDJSvistalayer/ewdjs/startEverthing.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-echo "Starting services as background processes..."
-cd /opt/ewdjs
-sudo nohup node startFederator > /var/log/raptor/federator.log 2>&1 &
-sudo nohup node ewdStart-raptor > /var/log/raptor/ewdjs.log 2>&1 &
-
